9197018826e603009030de43b738cefc2f41d5da
Allan Galdino <allan.g.galdino@outlook.com>
2020-11-01 16:11:50 -0300

    everything redux related refactored to contextAPI

---
+++ src/providers/cart/cart.provider.jsx
@@ -0,0 +1,62 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+import {
+  addItemToCart,
+  removeItemFromCart,
+  filterItemFromCart,
+  getCartItemsCount,
+  getTotal
+} from './cart.utils';
+
+export const CartContext = createContext({
+  hidden: true,
+  toggleHidden: () => { },
+  cartItems: [],
+  addItem: () => { },
+  removeItem: () => { },
+  clearItemFromCart: () => { },
+  cartItemsCount: 0,
+  total: 0,
+});
+
+const CartProvider = ({ children }) => {
+  const [hidden, setHidden] = useState(true);
+  const [cartItems, setCartItems] = useState([]);
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const addItem = item => setCartItems(addItemToCart(cartItems, item));
+
+  const removeItem = item => setCartItems(removeItemFromCart(cartItems, item));
+
+  const clearItemFromCart = item => setCartItems(filterItemFromCart(cartItems, item));
+
+  const toggleHidden = () => setHidden(!hidden);
+
+  useEffect(() => {
+    setCartItemsCount(getCartItemsCount(cartItems));
+  }, [cartItems]);
+
+  useEffect(() => {
+    setTotal(getTotal(cartItems));
+  }, [cartItems]);
+
+  return (
+    <CartContext.Provider
+      value={{
+        hidden,
+        toggleHidden,
+        cartItems,
+        addItem,
+        cartItemsCount,
+        removeItem,
+        clearItemFromCart,
+        total,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
